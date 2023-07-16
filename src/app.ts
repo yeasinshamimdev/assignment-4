8adadca249a7e704a41cc36abfc57b1231ce4470
yeasinshamimdev <yeasinshamim7@gmail.com>
2023-07-16 22:47:27 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/midleware/globalErrorHandler';
 import router from './app/routes';
 
 const app: Application = express();
 
 app.use(cors());
+app.use(cookieParser());
 
 // parser
 app.use(express.json());
@@ -14,5 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/', router);
 
 app.use(globalErrorHandler);
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
 
 export default app;
